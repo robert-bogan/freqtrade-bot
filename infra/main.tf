a55ffcb6413052edff8f239ee6d70466d45a71a3
MyCap <cap032959@gmail.com>
2025-05-22 15:46:02 +0100

    add providers

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = ">= 1.32.1"
+    }
+  }
+}
+
 provider "hcloud" {
   token = var.hcloud_token
 }
