826cf1ebe9ecab0b3d7201a2083f57dbe180ead4
MyCap <cap032959@gmail.com>
2025-05-22 17:13:52 +0100

    fix vars definitions

---
+++ infra/terraform.tfvars
@@ -0,0 +1,4 @@
+# hcloud_token = "your-hetzner-api-token"
+hcloud_token = var.hcloud_token
+# ssh_key_name = "your-ssh-key-name"
+ssh_key_name = var.ssh_key_name
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,19 @@
 variable "hcloud_token" {
   description = "Hetzner API token"
   type        = string
+  default     = var.hcloud_token
   sensitive   = true
 }
 
 variable "ssh_key_name" {
   description = "SSH key name in Hetzner Cloud"
+  default     = var.ssh_key_name
   type        = string
 }
 
 variable "client_ip" {
   description = "The IP address allowed to access the server"
   type        = string
+  default     = var.client_ip
+  sensitive   = true
 }
