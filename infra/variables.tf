a7100999cc22986834d48da6b7f666602790e650
MyCap <cap032959@gmail.com>
2025-05-22 17:15:48 +0100

    fix allowable variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,16 @@
 variable "hcloud_token" {
   description = "Hetzner API token"
   type        = string
-  default     = var.hcloud_token
   sensitive   = true
 }
 
 variable "ssh_key_name" {
   description = "SSH key name in Hetzner Cloud"
-  default     = var.ssh_key_name
   type        = string
 }
 
 variable "client_ip" {
   description = "The IP address allowed to access the server"
   type        = string
-  default     = var.client_ip
   sensitive   = true
 }
