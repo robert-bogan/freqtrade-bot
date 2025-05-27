bab94e6c6ac5f3ba639aedc5c18d97619cb5db29
MyCap <cap032959@gmail.com>
2025-05-27 11:31:16 +0100

    userdata revert

--- infra/main.tf
+++ infra/main.tf
@@ -73,6 +73,7 @@ user_data = <<-EOF
     # Initialize and mount gocryptfs as freqtrade
     - runuser -l freqtrade -c "gocryptfs -init /mnt/secure_raw || true"
     - runuser -l freqtrade -c "gocryptfs --extpass 'cat /home/freqtrade/.gocryptfs_pass' /mnt/secure_raw /mnt/secure"
+    # - chown -R freqtrade:freqtrade /mnt/secure/freqtrade-bot
 
 
     # Render config.json if template exists
@@ -92,6 +93,31 @@ user_data = <<-EOF
     - runuser -l freqtrade -c "cp /mnt/secure/freqtrade-bot/config/config.json /mnt/secure/freqtrade-bot/user_data/config.json"
     # - runuser -l freqtrade -c "chown -R freqtrade:freqtrade /mnt/secure/freqtrade-bot/user_data"
     # - runuser -l freqtrade -c "chmod 644 /mnt/secure/freqtrade-bot/user_data/config.json"
+
+    # Clone the repo as freqtrade user
+    - runuser -l freqtrade -c "git clone https://github.com/robert-bogan/freqtrade-bot.git /mnt/secure/freqtrade-bot"
+
+    # Create env config
+    - runuser -l freqtrade -c "mkdir -p /mnt/secure/freqtrade-bot/config"
+    - runuser -l freqtrade -c "echo "POSTGRES_PASSWORD=${var.postgres_password}" > /mnt/secure/freqtrade-bot/config/.env"
+
+    # Optional debug
+    - ls -la /mnt/secure/freqtrade-bot
+
+    # Render config.json if template exists
+    - runuser -l freqtrade -c "
+      |
+        cd /mnt/secure/freqtrade-bot
+        if [ -f config/config.json.template ]; then
+          export $(cat config/.env | xargs)
+          envsubst < config/config.json.template > config/config.json
+        fi
+      "
+        
+    # Set up config.json
+    - runuser -l freqtrade -c "cp /mnt/secure/freqtrade-bot/config/config.json /mnt/secure/freqtrade-bot/user_data/config.json"
+    - runuser -l freqtrade -c "chown -R freqtrade:freqtrade /mnt/secure/freqtrade-bot/user_data"
+    - runuser -l freqtrade -c "chmod 644 /mnt/secure/freqtrade-bot/user_data/config.json"
 
     # Clone the repo as freqtrade user
     - runuser -l freqtrade -c "git clone https://github.com/robert-bogan/freqtrade-bot.git /mnt/secure/freqtrade-bot"
